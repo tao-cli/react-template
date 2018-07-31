841c45f5e87b8554912aaa91bf3b1a9e069e540c
XuTao <xt01@8531.cn>
2018-07-31 09:15:02 +0800

    路由

--- src/modules/horizonGrid/index.jsx
+++ src/modules/horizonGrid/index.jsx
@@ -25,7 +25,7 @@ class horizonApp extends React.Component {
             style={{ lineHeight: "64px" }}
           >
             <Menu.Item key="1"><Link to='/app' >App</Link></Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="2"><Link to='/tab' >Tab</Link></Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
           </Menu>
         </Header>
--- src/modules/router/router.js
+++ src/modules/router/router.js
@@ -2,12 +2,15 @@ import {Switch, Route, Link } from 'react-router-dom';
 import React from "react";
 import { render } from "react-dom";
 
-import Home from "../app/App.js"
+import Home from "../home/home";
+import Table from "../table/table";
+
 class RouterIndex extends React.Component{
     render(){
         return <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/app" component={Home}/>
+            <Route path="/tab" component={Table}/>
         </Switch>
     }
 }
